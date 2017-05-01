14d70d0b18d2af5486a716f2a65b861d3ff789c8
Aaron Bikis <aaron.bikis@gmail.com>
2017-05-01 16:29:32 -0700

    Initial Commit

---
+++ SVNOnboardingViewController/SVNOnboardingViewController.h
@@ -0,0 +1,19 @@
+//
+//  SVNOnboardingViewController.h
+//  SVNOnboardingViewController
+//
+//  Created by Aaron Dean Bikis on 5/1/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNOnboardingViewController.
+FOUNDATION_EXPORT double SVNOnboardingViewControllerVersionNumber;
+
+//! Project version string for SVNOnboardingViewController.
+FOUNDATION_EXPORT const unsigned char SVNOnboardingViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNOnboardingViewController/PublicHeader.h>
+
+
